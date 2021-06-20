ce2711c58ca06265b9a4cbbe56b88ef6940f3bdf
Rico Putra Pradana <rico.putra@outlook.co.id>
2021-06-20 18:41:26 +0700

    Done Post API

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-  path('', views.UserList.as_view()),
-  path('<int:pk>/', views.UserDetail.as_view()),
+  path('users/', views.UserList.as_view()),
+  path('users/<int:pk>/', views.UserDetail.as_view()),
+  path('posts/', views.PostList.as_view()),
+  path('posts/<int:pk>/', views.PostDetail.as_view()),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from api import serializers
+from .models import Post
 from django.contrib.auth.models import User
 
 class UserList(generics.ListAPIView):
@@ -9,3 +10,21 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
   queryset = User.objects.all()
   serializer_class = serializers.UserSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+  """
+  Handler for `get` and `post` for a list of posts
+  """
+  queryset = Post.objects.all()
+  serializer_class = serializers.PostSerializer
+
+  def perform_create(self, serializer):
+    serializer.save(owner=self.request.user)
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+  """
+  Handler for `get`, `update`, and `delete` for a post
+  """
+  queryset = Post.objects.all()
+  serializer_class = serializers.PostSerializer
